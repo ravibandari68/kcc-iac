b29068da6ca4fe8dd987f6e7a88daddabd1bfe5e
Ravi Bandari <122573217+ravibandari68@users.noreply.github.com>
2025-09-23 09:25:44 +0530

    delete resources

--- env/non-prod/main.tf
+++ env/non-prod/main.tf
@@ -8,42 +8,42 @@ locals {
 
 ## Layer-0###
 ##################### VPC #####################
-module "vpc" {
-  source          = "../../modules/vpc"
-  name            = var.vpc_name
-  vpc_cidr        = var.vpc_cidr
-  azs             = var.azs
-  public_subnets  = var.public_subnets
-  private_subnets = var.private_subnets
-  tags            = var.tags
-}
+# module "vpc" {
+#   source          = "../../modules/vpc"
+#   name            = var.vpc_name
+#   vpc_cidr        = var.vpc_cidr
+#   azs             = var.azs
+#   public_subnets  = var.public_subnets
+#   private_subnets = var.private_subnets
+#   tags            = var.tags
+# }
 
 ## Layer-1###
 ##################### EC2 #####################
-module "ec2" {
-  source           = "../../modules/ec2"
- availability_zone   = var.azs[0]
-  name             = var.ec2_name
-  vpc_id           = module.vpc.vpc_id
-  subnet_ids       = module.vpc.public_subnet_ids
-  ami_id           = var.ec2_ami_id
-  user_data        = local.ec2_user_data
-  instance_count   = var.ec2_instance_count
-  ingress_rules    = var.ec2_ingress_rules
-  egress_rules     = var.ec2_egress_rules
-iam_policies = var.iam_policies
-assign_static_ip = true
+# module "ec2" {
+#   source           = "../../modules/ec2"
+#  availability_zone   = var.azs[0]
+#   name             = var.ec2_name
+#   vpc_id           = module.vpc.vpc_id
+#   subnet_ids       = module.vpc.public_subnet_ids
+#   ami_id           = var.ec2_ami_id
+#   user_data        = local.ec2_user_data
+#   instance_count   = var.ec2_instance_count
+#   ingress_rules    = var.ec2_ingress_rules
+#   egress_rules     = var.ec2_egress_rules
+# iam_policies = var.iam_policies
+# assign_static_ip = true
 
-}
+# }
 ##################### ECR #####################
- module "ecr" {
-  source = "../../modules/ecr"
-  repository_names     = var.ecr_repository_names
-  image_tag_mutability = "MUTABLE"
-  scan_on_push         = true
-  tags = var.tags
+#  module "ecr" {
+#   source = "../../modules/ecr"
+#   repository_names     = var.ecr_repository_names
+#   image_tag_mutability = "MUTABLE"
+#   scan_on_push         = true
+#   tags = var.tags
 
-}
+# }
 
 ##################### ALB #####################
 
